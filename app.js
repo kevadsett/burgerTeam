eeb7e577aa825fb0d0942519bbec62c9415a737d
Kev Adsett <kev.adsett@gmail.com>
2015-04-02 14:30:23 +0100

    Setup basic node server for game

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
